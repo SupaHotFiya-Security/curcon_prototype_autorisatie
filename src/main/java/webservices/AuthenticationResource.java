5ce6c4659653c2dabb8a7508b3bb1e8c09a1f1a1
Martinj98 <martin.dejongh@student.hu.nl>
2018-04-19 22:33:50 +0200

    Signout+currentuser added

--- src/main/java/webservices/AuthenticationResource.java
+++ src/main/java/webservices/AuthenticationResource.java
@@ -1,25 +1,25 @@
 package webservices;
 
 ///import java.awt.Window;
-import java.security.Key;
+//import java.security.Key;
 //import java.awt.PageAttributes.MediaType;
 //import java.awt.RenderingHints.Key;
-import java.util.Calendar;
-
-import javax.ws.rs.Consumes;
-import javax.ws.rs.FormParam;
+//import java.util.Calendar;
+//
+//import javax.ws.rs.Consumes;
+//import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
+//import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-
-import io.jsonwebtoken.JwtException;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.impl.crypto.MacProvider;
+//import javax.ws.rs.core.Response;
+//
+//import io.jsonwebtoken.JwtException;
+//import io.jsonwebtoken.Jwts;
+//import io.jsonwebtoken.SignatureAlgorithm;
+//import io.jsonwebtoken.impl.crypto.MacProvider;
 import persistence.UserDAO;
 
 @Path("/authentication")
@@ -32,7 +32,6 @@ public class AuthenticationResource {
 	// MacProvider.generateKey();PPLICATION_FORM_URLENCODED)
 	public String authenticateUser(@PathParam("email") String email) {
 		// try {
-		System.out.println("ianfi");
 		// Authenticate the user against the database
 		UserDAO dao = new UserDAO();
 		String role = dao.findRoleForUsernameAndPassword(email);
